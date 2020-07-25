b2b3f26ca39a3101677b8abb66bd148cb7ae47b3
Nishantjain719 <nishant.jain@asu.apeejay.edu>
2020-07-25 11:44:51 +0530

    add widegt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Accordian from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+    {
+        title: 'A',
+        content: 'Hello'
+    },
+    {
+        title: 'B',
+        content: 'Modi'
+    },
+    {
+        title: 'C',
+        content: 'Trump'
+    }
+];
 
-export default App;
+const options = [
+    {
+        label: 'The Red',
+        value: 'red'
+    },
+    {
+        label: 'The Green',
+        value: 'green'  
+    },
+    {
+        label: 'The Blue',
+        value: 'blue'
+    }
+];
+
+export default () => {
+    const [selected, setSelected] = useState(options[0]);
+   return (
+       <div>
+           <Header />
+          <Route path="/">
+              <Accordian items={items} />
+              </Route>
+              <Route path="/list">
+                  <Search />
+              </Route>
+              <Route path="/dropdown">
+                  <Dropdown
+                  label="Select a Color"
+                  options={options}
+                  selected={selected}
+                  onSelectedChange={setSelected} 
+                  />
+              </Route>
+              <Route path="/translate">
+                  <Translate />
+              </Route>
+       </div>
+   );
+};
